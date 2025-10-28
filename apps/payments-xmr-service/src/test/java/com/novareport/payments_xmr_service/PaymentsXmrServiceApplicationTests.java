34be639151ee6e97d05a3f64a65f1776ec5cd082
david-andreasson <79davand@gafe.molndal.se>
2025-10-28 16:05:53 +0100

    Initialized folder structure with seperated microservices

---
+++ apps/payments-xmr-service/src/test/java/com/novareport/payments_xmr_service/PaymentsXmrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novareport.payments_xmr_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsXmrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ apps/reporter-service/src/main/java/com/novareport/reporter_service/ReporterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.novareport.reporter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporterServiceApplication.class, args);
+	}
+
+}
